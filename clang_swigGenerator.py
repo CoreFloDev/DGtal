f06618bafe5a953fb0f869c042a2d11c302ca170
tornoz <tornoz@laposte.net>
2015-02-23 21:53:17 +0100

    [ADD]generator script

--- clang_swigGenerator.py
+++ clang_swigGenerator.py
@@ -1,2 +1,106 @@
 #!/usr/bin/python2
-#This will be the python script generating the SWIG interfaces
+# vim: set fileencoding=utf-8
+import clang.cindex
+import sys
+import os
+import pprint
+import traceback
+from mako.template import Template
+
+LIB_CLANG = '/usr/lib/libclang.so'
+MAKO_PATH = 'src/SWIG/swig.mako'
+OUTPUT_FILE = 'src/SWIG/dgtal_output.i'
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+# function to parse AST
+def traverse(node,parent,filename):
+
+    ret = {}
+
+    try:
+        # do compute here
+        if node.location.file.name == filename:
+
+            # All templates
+            if node.kind == CK.CLASS_TEMPLATE:
+                parameterOfTemplate = [] 
+                for child in node.get_children():
+                    if child.kind in [CK.TEMPLATE_TYPE_PARAMETER,CK.TEMPLATE_TEMPLATE_PARAMETER,CK.TEMPLATE_NON_TYPE_PARAMETER]:
+                        parameterOfTemplate.append(child.displayname)
+                ret[node.spelling] = parameterOfTemplate
+
+    except:
+        pass
+
+    for child_node in node.get_children():
+        trav = traverse(child_node,node,filename)
+        ret = dict(ret.items() + trav.items())
+        
+        
+    return ret
+#ned traverse
+
+if len(sys.argv) < 2:
+    print("Usage: dump_ast.py header_file [output_file]")
+    sys.exit()
+
+# load Clang
+clang.cindex.Config.set_library_file(LIB_CLANG)
+index = clang.cindex.Index.create()
+
+
+allowedExt = ['.h','.cpp','.ih']
+CK = clang.cindex.CursorKind
+
+
+print bcolors.OKGREEN+"--- Starting swig generation ---"+bcolors.ENDC
+
+# only one source file
+sourcePath = sys.argv[1]
+translation_unit = index.parse(sourcePath, ['-x', 'c++', '-std=c++11', '-D__CODE_GENERATOR__'])
+templateFound = traverse(translation_unit.cursor,None,sourcePath)
+templates = []
+includes = [sourcePath]
+
+# generate the swig interface
+tpl = Template(filename=MAKO_PATH)
+output = tpl.render(templates=templates,
+                    includes=includes)
+print output
+
+output = OUTPUT_FILE
+if len(sys.argv) > 2:
+    output = sys.argv[2]
+ 
+f = open( output,'w')
+f.write(output)
+f.close()
+
+## list of all file of DGTal
+#for dirname, dirnames, filenames in os.walk(sys.argv[1]):
+
+#    # print path to all filenames.
+#    for filename in filenames:
+#        if filename.endswith(tuple(allowedExt)):
+#            sourcePath = os.path.join(dirname, filename)
+#            # print sourcePath
+#            print sourcePath
+#            translation_unit = index.parse(sourcePath, ['-x', 'c++', '-std=c++11', '-D__CODE_GENERATOR__'])
+#            traverse(translation_unit.cursor,None,sourcePath)
+
+#    # Advanced usage:
+#    # editing the 'dirnames' list will stop os.walk() from recursing into there.
+#    if '.git' in dirnames:
+#        # don't go into any .git directories.
+#        dirnames.remove('.git')
+#    
+#    if 'doc' in dirnames:
+#        dirnames.remove('doc')
